f0325d23e5c2e2b937410819607042d6dd0a2534
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-01-15 13:15:22 +0530

    prime number fnx

--- urlShortener/primesInRange.java
+++ urlShortener/primesInRange.java
@@ -10,10 +10,10 @@ public class primesInRange{
         }
     }
     public static boolean isPrime(int num) {
-        if(num == 2) {
-            return true;
+        if(num < 2) {
+            return false;
         }
-        for(int i=2; i<=num; i++) {
+        for(int i=2; i*i<=num; i++) {
             if(num % i == 0) return false;
         }
         return true;
