79df8b5d353726312ce29a2e5028a78ee5ac830b
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-01-16 13:13:39 +0530

    java bug fixed

--- urlShortener/primesInRange.java
+++ urlShortener/primesInRange.java
@@ -3,6 +3,7 @@ public class primesInRange{
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.close();
         for(int i=1; i<=num; i++) {
             if(isPrime(i)) {
                 System.out.println(i);
